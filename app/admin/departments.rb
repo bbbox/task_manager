d99184791c42cf56a8f19a59b25b8bd6634826b2
Mihail Evsyukov <evsyukovmv@gmail.com>
2012-04-29 04:52:43 +0300

    russian

--- app/admin/departments.rb
+++ app/admin/departments.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register Department do
-  
+  menu :label => I18n.t('active_admin.departments')
+  filter :name, label: I18n.t('active_admin.by_name')
+  filter :chief, label: I18n.t('active_admin.chief')
+
+  form do |f|
+    f.inputs I18n.t('active_admin.department') do
+      f.input :name, label: I18n.t('active_admin.name')
+      f.input  :chief, label: I18n.t('active_admin.chief')
+    end
+    f.buttons
+  end
+
+  index do
+    column I18n.t('active_admin.name'), :name
+    column I18n.t('active_admin.chief'), :chief
+    default_actions
+  end
 end
